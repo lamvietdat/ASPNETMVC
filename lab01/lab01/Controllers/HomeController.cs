485c316b113e21f1e5114f44aa26e13a9fb584b5
lamvietdat <git@github.com:lamvietdat/ASP.NET.git>
2024-05-27 11:01:26 +0700

    Update

---
+++ lab01/lab01/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: /Home/About
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.Message = "Research IT - DEVMASTER / Chúng tôi sẽ làm bạn hài lòng!";
+            return View();
+        }
+        //GET: Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thông tin liên hệ";
+            return View();
+        }
+        //GET: Home/Products
+        public ActionResult Products()
+        {
+             ViewBag.Message = "Thực đơn hôm nay";
+             string[] products = {"Gà quay","Cá chép om dưa","Vịt quay vân đình","Ốc xào xả ớt"};
+             ViewBag.products = products;
+             return View();
+        }
+    }
+}
---
+++ lab01/lab01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab01.MvcApplication" Language="C#" %>
